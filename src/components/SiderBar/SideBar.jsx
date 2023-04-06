66c2b606bee5ffbbe7d4ebf071f424a889d10197
Havochalo <officialmail85@gmail.com>
2023-04-06 19:54:15 +1000

    SideBar created

--- src/components/SiderBar/SideBar.jsx
+++ src/components/SiderBar/SideBar.jsx
@@ -17,6 +17,24 @@ export const SideBar = () => {
           developer journey.
         </p>
       </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="categoriesList">
+          <li className="sidebarListItem">Lifestyle</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Programming</li>
+          <li className="sidebarListItem">AI</li>
+          <li className="sidebarListItem">Immigrant Journey</li>
+        </ul>
+      </div>
+
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <div className="sidebarSocail">
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-square-github"></i>
+        </div>
+      </div>
     </div>
   );
 };
